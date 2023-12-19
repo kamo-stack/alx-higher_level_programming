9385b1fde80ea2738c2582f24aba5df6b8624400
Kamogelo Ashley <kamoashley33@gmail.com>
2023-12-19 14:35:28 -0800

    6-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -7,11 +8,13 @@ class Square:
 
     def __init__(self, size):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
         self.size = size
- @property
+
+    @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
@@ -29,7 +32,7 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-          """Print the square with the # character."""
+        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
