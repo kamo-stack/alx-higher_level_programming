decdf44f55c18520935365f28d5eaddca5f2bfe2
Kamo Ashley <kamoashley33@gmail.com>
2024-04-25 15:25:52 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,15 +1,9 @@
-
 #!/usr/bin/node
+// writes a string to a file
 
 const fs = require('fs');
-// Import the built-in Node.js 'fs' module.
-
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
-  // Use fs.writeFile() to write data to a file specified as the third command-line argument (process.argv[2]).
-  // The data to be written is taken from the fourth command-line argument (process.argv[3]).
-
-  if (error) {
-    // If an error occurs during the write operation, the 'error' parameter will contain an error object.
-    console.error(error);
-  }
-});
+if (process.argv.length > 3) {
+  fs.writeFile(process.argv[2], process.argv[3], x => {
+    if (x) console.log(x);
+  });
+}
